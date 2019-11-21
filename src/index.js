f52432bf10d228025a07ea3a5f5f7d4576b30b7d
Ives Bao <ives.bao@softtek.com>
2019-11-21 17:52:50 +0800

    modify title&ico

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+document.titile="富豪模拟器";
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
